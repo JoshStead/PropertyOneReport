954466517e6cfbfb19b57ef0149cf179d964ce7b
JoshStead <53786642+JoshStead@users.noreply.github.com>
2026-02-04 22:35:09 +0000

    Rest of the files

---
+++ config.py
@@ -0,0 +1,16 @@
+# API Configuration
+# Store your API keys here
+
+# EasyPostcodes API Configuration
+# Sign up at: https://easypostcodes.com/
+EASYPOSTCODES_API_KEY = 'NCMMlxL5Dtm3rGlNQpzn7gU0qQRrS16vZLewHHRI'
+
+# EPC API Configuration
+# Sign up at: https://epc.opendatacommunities.org/
+EPC_API_KEY = '8cdf6e96457c55fd8522c74f31127843f953b40e'
+EPC_EMAIL = 'steedy27@gmail.com'  # Email used to sign up for EPC API
+
+# TransportAPI Configuration
+# Sign up at: https://www.transportapi.com/
+TRANSPORTAPI_APP_ID = '605d2c10'
+TRANSPORTAPI_APP_KEY = '32725adee298bfdfcf162467d569de77'
